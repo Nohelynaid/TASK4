6c404e331701734e4454de754e6a045342d974b2
Nohelynaid <101210341+Nohelynaid@users.noreply.github.com>
2025-08-04 16:45:29 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
+
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
-module.exports = pool;
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
